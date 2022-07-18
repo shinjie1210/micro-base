6753c874126153007a68d73988e6fe84ec7c1e1d
沈兴宇 <shinjie1210@foxmail.com>
2022-07-18 18:15:56 +0800

    update

--- src/views/son.vue
+++ src/views/son.vue
@@ -6,7 +6,7 @@
       url(必传)：应用地址，会被自动补全
       baseroute(可选)：基座应用分配给子应用的基础路由，就是上面的 `/son`
      -->
-        <micro-app name="son" :url="url">
+        <micro-app name="son" :url="url" baseroute="/base/son">
         </micro-app>
     </div>
 </template>
